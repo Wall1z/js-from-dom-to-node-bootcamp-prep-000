3e548be0e18445fed9896c5081b7207308afe61f
wael alnajar <w_alnajar@yahoo.com>
2017-09-15 20:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ console.log("Hello, World!")
 console.log("hi this is my first program in js")
 var dog = "thirsty"
 
-if (dog === "hungry") {
+if (dog === "hungry, thir") {
   console.log("Refilling food bowl.")
 } else if (dog === "thirsty") {
   console.log("Refilling water bowl.")
 } else {
   console.log("Reading newspaper.")
   }
+  var name = "Joe"
+  var height = "74"
+  if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
