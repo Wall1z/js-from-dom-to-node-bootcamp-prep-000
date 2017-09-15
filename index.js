f710d5fdcea94288e3839593c1a28516ead2aa9a
wael alnajar <w_alnajar@yahoo.com>
2017-09-15 20:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // write your code below!
 console.log("Hello, World!")
 console.log("hi this is my first program in js")
+var dog = "thirsty"
+ 
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+} else {
+  console.log("Reading newspaper.")
+  
